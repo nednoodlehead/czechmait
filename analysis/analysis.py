62a68c1d7f22ff6e4dca722afe5f3decad75a006
NedNoodleHead <spencer.braak@gmail.com>
2023-02-15 16:50:49 -0800

    added available moves, does not work though

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -41,6 +41,13 @@ class Tile:
         return f"{self.tile}"
 
 
+# represents an empty tile
+class Empty:
+    # used in the queries of what occupies a tile, returns None to that query. This is the alternative to error handing
+    # within the queries themselves
+    color = None
+
+
 class ChessBoard:
     score = 0
 
@@ -59,16 +66,28 @@ class ChessBoard:
         :param color: str, either white or black, the color of the player querying
         :returns bool: is it occupied by enemy piece? aka, can it be taken?"""
         if color.lower() == "black":
-            if self.board[tile].color:
-                pass
-        if color.lower == "white":
-            if self.board[tile].color:
-                pass
+            if self.board[tile].color == "white":
+                return True
+            else:
+                return False
+        elif color.lower() == "white":
+            if self.board[tile].color == "black":
+                return True
+            else:
+                return False
         else:
-            raise ValueError(f"Did not input black or white :: {color}")
+            # raise ValueError(f"Did not input black or white :: {color}")
+            return "balls?"
 
     def testin(self):
-        print(self.board["a1"])
+        print(self.board["a1"].color)
+
+    def get_tile(self, tile):
+        """Returns the desired tile instance from given input
+        :param tile (str) tile instance being requested"""
+        for key in self.board.keys():
+            if key.tile == tile:
+                return key
 
 
 class Piece:
@@ -91,6 +110,8 @@ class Pawn(Piece):
 
     def __init__(self, color, starting_tile):
         super().__init__(color, starting_tile)
+        self.color = color
+        self.starting_tile = starting_tile
 
     def available_moves(self, tile: Tile, board: ChessBoard):  # returns a move notation list e.g. [a4, xb3]
         # tile = b3, h5
@@ -103,7 +124,7 @@ class Pawn(Piece):
         # if the color is black
         if self.color == "black":
             # and it is on its default rank
-            if tile[0] == "7":
+            if tile == self.starting_tile:
                 # if the letter position two ahead of it is unoccupied, you can move there
                 if not board.is_occupied(f"{tile[0]}5"):
                     # add to available moves list
@@ -114,7 +135,11 @@ class Pawn(Piece):
                 if not board.is_occupied(f"{tile[0]}{int(tile[1] -1)}"):
                     available.append(f"{tile[0]}{int(tile[1] -1)}")
             # if there is an opposite colored piece that is on its diagonals
-            # if tile.convert_tile(1, -1)
+            if board.is_occupied_enemy(tile.convert_tile(1, -1), self.color):
+                available.append(f"{tile[0]}x{tile.convert_tile(1, -1)}")
+            if board.is_occupied_enemy(tile.convert_tile(1, 1), self.color):
+                available.append(f"{tile[0]}x{tile.convert_tile(1, 1)}")
+        return available
 
 
 class Rook(Piece):
@@ -153,32 +178,35 @@ class King(Piece):
 
 
 default_board = {
-    Tile("a1"): None, Tile("a2"): None, Tile("a3"): None,
-    Tile("a4"): None, Tile("a5"): None, Tile("a6"): None,
-    Tile("a7"): None, Tile("a8"): None, Tile("b1"): None,
-    Tile("b2"): None, Tile("b3"): None, Tile("b4"): None,
-    Tile("b5"): None, Tile("b6"): None, Tile("b7"): None,
-    Tile("b8"): None, Tile("c1"): None, Tile("c2"): None,
-    Tile("c3"): None, Tile("c4"): None, Tile("c5"): None,
-    Tile("c6"): None, Tile("c7"): None, Tile("c8"): None,
-    Tile("d1"): None, Tile("d2"): None, Tile("d3"): None,
-    Tile("d4"): None, Tile("d5"): None, Tile("d6"): None,
-    Tile("d7"): None, Tile("d8"): None, Tile("e1"): None,
-    Tile("e2"): None, Tile("e3"): None, Tile("e4"): None,
-    Tile("e5"): None, Tile("e6"): None, Tile("e7"): None,
-    Tile("e8"): None, Tile("f1"): None, Tile("f2"): None,
-    Tile("f3"): None, Tile("f4"): None, Tile("f5"): None,
-    Tile("f6"): None, Tile("f7"): None, Tile("f8"): None,
-    Tile("g1"): None, Tile("g2"): None, Tile("g3"): None,
-    Tile("g4"): None, Tile("g5"): None, Tile("g6"): None,
-    Tile("g7"): None, Tile("g8"): None, Tile("h1"): None,
-    Tile("h2"): None, Tile("h3"): None, Tile("h4"): None,
-    Tile("h5"): None, Tile("h6"): None, Tile("h7"): None,
-    Tile("h8"): None,
+    Tile("a1"): Rook("white", "a1"), Tile("a2"): Pawn("white", "a2"), Tile("a3"): Empty(),
+    Tile("a4"): Empty(), Tile("a5"): Empty(), Tile("a6"): Empty(),
+    Tile("a7"): Pawn("black", "a7"), Tile("a8"): Rook("black", "a7"), Tile("b1"): Knight("white", "b1"),
+    Tile("b2"): Pawn("white", "b2"), Tile("b3"): Empty(), Tile("b4"): Empty(),
+    Tile("b5"): Empty(), Tile("b6"): Empty(), Tile("b7"): Pawn("black", "b7"),
+    Tile("b8"): Knight("black", "b8"), Tile("c1"): Bishop("white", "c1"), Tile("c2"): Pawn("white", "c2"),
+    Tile("c3"): Empty(), Tile("c4"): Empty(), Tile("c5"): Empty(),
+    Tile("c6"): Empty(), Tile("c7"): Pawn("black", "c7"), Tile("c8"): Bishop("black", "c8"),
+    Tile("d1"): Queen("white", "d1"), Tile("d2"): Pawn("white", "d2"), Tile("d3"): Empty(),
+    Tile("d4"): Empty(), Tile("d5"): Empty(), Tile("d6"): Empty(),
+    Tile("d7"): Pawn("black", "d7"), Tile("d8"): Queen("black", "d8"), Tile("e1"): King("white", "e1"),
+    Tile("e2"): Pawn("white", "e2"), Tile("e3"): Empty(), Tile("e4"): Empty(),
+    Tile("e5"): Empty(), Tile("e6"): Empty(), Tile("e7"): Pawn("white", "e7"),
+    Tile("e8"): King("black", "e8"), Tile("f1"): Bishop("white", "f1"), Tile("f2"): Pawn("white", "f2"),
+    Tile("f3"): Empty(), Tile("f4"): Empty(), Tile("f5"): Empty(),
+    Tile("f6"): Empty(), Tile("f7"): Pawn("black", "f7"), Tile("f8"): Bishop("black", "f8"),
+    Tile("g1"): Knight("white", "g1"), Tile("g2"): Pawn("white", "g2"), Tile("g3"): Empty(),
+    Tile("g4"): Empty(), Tile("g5"): Empty(), Tile("g6"): Empty(),
+    Tile("g7"): Pawn("black", "g7"), Tile("g8"): Knight("black", "g8"), Tile("h1"): Rook("white", "h1"),
+    Tile("h2"): Pawn("white", "h2"), Tile("h3"): Empty(), Tile("h4"): Empty(),
+    Tile("h5"): Empty(), Tile("h6"): Empty(), Tile("h7"): Pawn("black", "h7"),
+    Tile("h8"): Rook("black", "h8"),
     }
 
 chs = ChessBoard()
-chs.testin()
+# x = chs.is_occupied_enemy(chs.get_tile("g5"), "black")
+x = chs.board["c7"].available_moves(chs.get_tile("c7"), chs)
+
+print(x)
 
 
 
