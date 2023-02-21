c61909f7251e2d882044fcf71a7f7070d682e0ec
NedNoodleHead <spencer.braak@gmail.com>
2023-02-20 18:01:48 -0800

    Empty class
    Placed here so multiple files can access it easier

---
+++ empty/empty.py
@@ -0,0 +1,4 @@
+class Empty:
+    # used in the queries of what occupies a tile, returns None to that query. This is the alternative to error handing
+    # within the queries themselves
+    color = None
