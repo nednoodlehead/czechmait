7030298bfd9b966df12fa376ded2b902d1456bfe
NedNoodleHead <spencer.braak@gmail.com>
2023-02-20 17:49:22 -0800

    fixes to png conversion

--- image/export.py
+++ image/export.py
@@ -2,13 +2,10 @@ from PIL import Image
 import os.path
 from empty.empty import Empty
 
-# os.chdir(r"F:\Projects\Python Projects\czechmait")
 
 def generate_map():
     piece_map = {}
-    print(os.getcwd())
     for path in os.listdir(r"./resources"):
-        print(path)
         opened = Image.open(f"./resources/{path}")
         png = opened.convert("RGBA")
         piece_map[path[:-4]] = png
@@ -20,7 +17,6 @@ def hashmap_to_png(board: dict):
     png_board = Image.open("./resources/chessboard.png")
     for key, value in board.items():
         if not isinstance(value, Empty):
-            print(value)
             y_value, x_value = tile_to_pixels(key.tile)
             png_board.paste(piece_key[value.name], (y_value, x_value), piece_key[value.name])
     png_board.save("OUTPUT.png")
@@ -38,7 +34,17 @@ def tile_to_pixels(tile: str) -> (int, int):
         "g": 600,
         "h": 700
     }
-    return char_map[tile[0]], (int(tile[1]) - 1) * 100
+    num_map = {
+        1: 700,
+        2: 600,
+        3: 500,
+        4: 400,
+        5: 300,
+        6: 200,
+        7: 100,
+        8: 0
+    }
+    return char_map[tile[0]], num_map[int(tile[1])]
 
 
 
