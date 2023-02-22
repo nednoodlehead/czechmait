782ff5b6e3a383656cd246771a1e6ff0fbeabb53
NedNoodleHead <spencer.braak@gmail.com>
2023-02-22 11:27:01 -0800

    added en passant to notation translation

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -154,12 +154,21 @@ class ChessBoard:
             new_tile = notation[2:]
             # if it is white, mark the tile above and on the first-letter file
             if turn == "white":
-                # represents the only tile on the board that a pawn could take a white piece on this square
                 start_tile = notation[0] + str(int(notation[-1]) - 1)
+                # if the tile that is being taken has no piece, an espassant occured
+                if not self.is_occupied(new_tile):
+                    # if en passant is occuring, we return the tile being caputured, but also the update for the tile
+                    # that the piece dies on, which for white is -1 on numbers, -0 on letters
+                    # it will be parsed later that this extra arguement will set that tile to empty
+                    return (start_tile, new_tile, return_type), (self.convert_tile(new_tile, 0, -1), Empty)
+                    # represents the only tile on the board that a pawn could take a white piece on this square
                 return start_tile, new_tile, return_type
             # if the color is black (all other cases were removed on the valueError above
             else:
+                # start tile for black, black plays downwards
                 start_tile = notation[0] + str(int(notation[-1]) + 1)
+                if not self.is_occupied(new_tile):
+                    return (start_tile, new_tile, return_type), (self.convert_tile(new_tile, 0, 1), Empty)
             return start_tile, new_tile, return_type
         # there is no taking notation, it is a pawn move. e.g. b3
         else:
@@ -551,36 +560,34 @@ testing_board = {
     Tile("h8"): Rook("black", "h8"),
     }
 
-castle_board = {
+enpass =     enpassant = {
     Tile("a1"): Rook("white", "a1"), Tile("a2"): Pawn("white", "a2"), Tile("a3"): Empty(),
     Tile("a4"): Empty(), Tile("a5"): Empty(), Tile("a6"): Empty(),
     Tile("a7"): Pawn("black", "a7"), Tile("a8"): Rook("black", "a7"), Tile("b1"): Knight("white", "b1"),
     Tile("b2"): Pawn("white", "b2"), Tile("b3"): Empty(), Tile("b4"): Empty(),
     Tile("b5"): Empty(), Tile("b6"): Empty(), Tile("b7"): Pawn("black", "b7"),
-    Tile("b8"): Empty(), Tile("c1"): Bishop("white", "c1"), Tile("c2"): Pawn("white", "c2"),
+    Tile("b8"): Knight("black", "b8"), Tile("c1"): Bishop("white", "c1"), Tile("c2"): Pawn("white", "c2"),
     Tile("c3"): Empty(), Tile("c4"): Empty(), Tile("c5"): Empty(),
-    Tile("c6"): Empty(), Tile("c7"): Pawn("black", "c7"), Tile("c8"): Empty(),
+    Tile("c6"): Empty(), Tile("c7"): Pawn("black", "c7"), Tile("c8"): Bishop("black", "c8"),
     Tile("d1"): Empty(), Tile("d2"): Pawn("white", "d2"), Tile("d3"): Empty(),
-    Tile("d4"): Empty(), Tile("d5"): Empty(), Tile("d6"): Empty(),
-    Tile("d7"): Pawn("black", "d7"), Tile("d8"): Empty(), Tile("e1"): King("white", "e1"),
-    Tile("e2"): Pawn("white", "e2"), Tile("e3"): Empty(), Tile("e4"): Empty(),
-    Tile("e5"): Empty(), Tile("e6"): Empty(), Tile("e7"): Pawn("white", "e7"),
-    Tile("e8"): King("black", "e8"), Tile("f1"): Empty(), Tile("f2"): Pawn("white", "f2"),
+    Tile("d4"): Empty(), Tile("d5"): Pawn("black", "e7"), Tile("d6"): Empty(),
+    Tile("d7"): Pawn("black", "d7"), Tile("d8"): Queen("black", "d8"), Tile("e1"): King("white", "e1"),
+    Tile("e2"): Empty(), Tile("e3"): Empty(), Tile("e4"): Empty(),
+    Tile("e5"): Pawn("white", "e2"), Tile("e6"): Empty(), Tile("e7"): Empty(),
+    Tile("e8"): King("black", "e8"), Tile("f1"): Bishop("white", "f1"), Tile("f2"): Pawn("white", "f2"),
     Tile("f3"): Empty(), Tile("f4"): Empty(), Tile("f5"): Empty(),
     Tile("f6"): Empty(), Tile("f7"): Pawn("black", "f7"), Tile("f8"): Bishop("black", "f8"),
-    Tile("g1"): Empty(), Tile("g2"): Pawn("white", "g2"), Tile("g3"): Empty(),
+    Tile("g1"): Knight("white", "g1"), Tile("g2"): Pawn("white", "g2"), Tile("g3"): Empty(),
     Tile("g4"): Empty(), Tile("g5"): Empty(), Tile("g6"): Empty(),
     Tile("g7"): Pawn("black", "g7"), Tile("g8"): Empty(), Tile("h1"): Rook("white", "h1"),
     Tile("h2"): Pawn("white", "h2"), Tile("h3"): Empty(), Tile("h4"): Empty(),
     Tile("h5"): Empty(), Tile("h6"): Empty(), Tile("h7"): Pawn("black", "h7"),
     Tile("h8"): Rook("black", "h8"),
-}
+    }
 
-chs = ChessBoard(castle_board)
+chs = ChessBoard(enpass)
 
-x = chs.notation_translation("O-O-O", "black")
-
+x = chs.notation_translation("exd6", "white")
 print(x)
-
 
 
