3164409c3ad655c36f9b33142becc2ea1b2904aa
NedNoodleHead <spencer.braak@gmail.com>
2023-03-04 11:34:28 -0800

    added en passant remnant

--- board/pieces.py
+++ board/pieces.py
@@ -4,12 +4,6 @@ class Piece:
         self.color = color
         self.starting_tile = starting_tile
 
-    def on_starting_tile(self, tile):
-        """ Returns a bool depending on the current tile vs original starting tile"""
-        if tile.tile == self.starting_tile:
-            return True
-        return False
-
     def __repr__(self):
         return f"starting: {self.starting_tile}"
 
@@ -23,37 +17,22 @@ class Pawn(Piece):
         super().__init__(color, starting_tile)
         self.color = color
         self.name = f"{color}_pawn"
-        self.starting_tile = starting_tile
 
-    def available_moves(self, tile, board):  # returns a move notation list e.g. [a4, xb3]
-        # tile = b3, h5
-        # board = hashmap to represent the entire board
-        # (black) if is on x7, can move two ahead
-        # if it is not, it may move one ahead
-        # rember to do ENPASSANT ?
-        # if the tile to its front left or right is occupied by opposite color piece, it is able to take
-        available = []
-        # if the color is black
-        if self.color == "black":
-            # and it is on its default rank
-            if tile == self.starting_tile:
-                # if the letter position two ahead of it is unoccupied, you can move there
-                print(board.is_occupied("f5"))
-                if not board.is_occupied(f"{tile[0]}5"):
-                    # add to available moves list
-                    available.append(f"{tile[0]}5")
-                    available.append(f"{tile[0]}6")
-            # if the given tile is not on the seventh rank
-            else:
-                # if the tile infront of it is unoccupied, you can move there
-                if not board.is_occupied(f"{tile[0]}{int(tile[1] -1)}"):
-                    available.append(f"{tile[0]}{int(tile[1] -1)}")
-            # if there is an opposite colored piece that is on its diagonals
-            if board.is_occupied_enemy(tile.convert_tile(1, -1), self.color):
-                available.append(f"{tile[0]}x{tile.convert_tile(1, -1)}")
-            if board.is_occupied_enemy(tile.convert_tile(1, 1), self.color):
-                available.append(f"{tile[0]}x{tile.convert_tile(1, 1)}")
-        return available
+
+class EnpassantRemnant:  # does not inherit from 'Piece', because it is logically not a piece, and to distinguish from
+    # normal pieces
+
+    # purpose of this class is to leave this behind in the square that was jumped over during a double jump pawn move
+    # it must have special properties to not have it block sightlines for other pieces, but be visible to pawns
+    # it must also decay one move after being placed
+    value = 1  # value = 1 cause it is a pawn capture...
+    # decay of all enpassant remnants will be reduced by 1 each turn, and will be removed if it == 0
+    # one is removed on said turn i believe, so 3 should make sense
+    decay = 3
+
+    def __init__(self, color):
+        self.color = color
+        self.name = f"{color}_en"
 
 
 class Rook(Piece):
