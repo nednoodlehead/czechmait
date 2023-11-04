ebe37108d03700f02a6e46c3680b89bd4d193da2
Neddy <nedninja.spencer@gmail.com>
2023-11-04 14:30:14 -0700

    changed structure for holding certain boards related to testing

--- tests/boards.py
+++ tests/boards.py
@@ -2,7 +2,6 @@ from board.pieces import Rook, Pawn, EnpassantRemnant, Bishop, Knight, Queen, Ki
 from board.empty import Empty
 from board.tile import Tile
 from board.color import Black, White
-
 default_board = {
     Tile("a1"): Rook(White), Tile("a2"): Pawn(White), Tile("a3"): Empty(),
     Tile("a4"): Empty(), Tile("a5"): Empty(), Tile("a6"): Empty(),
@@ -177,3 +176,77 @@ mate_in_5 = {
              Tile("h5"): Empty(), Tile("h6"): Empty(), Tile("h7"): Empty(),
              Tile("h8"): Empty()
             }
+
+test_enpassant_board = {
+        Tile("a1"): Rook(White), Tile("a2"): Pawn(White), Tile("a3"): Empty(),
+        Tile("a4"): Empty(), Tile("a5"): Empty(), Tile("a6"): Empty(),
+        Tile("a7"): Pawn(Black), Tile("a8"): Rook(Black), Tile("b1"): Knight(White),
+        Tile("b2"): Pawn(White), Tile("b3"): Empty(), Tile("b4"): Empty(),
+        Tile("b5"): Empty(), Tile("b6"): Empty(), Tile("b7"): Pawn(Black),
+        Tile("b8"): Knight(Black), Tile("c1"): Bishop(White), Tile("c2"): Pawn(White),
+        Tile("c3"): Empty(), Tile("c4"): Empty(), Tile("c5"): Empty(),
+        Tile("c6"): Empty(), Tile("c7"): Pawn(Black), Tile("c8"): Bishop(Black),
+        Tile("d1"): Empty(), Tile("d2"): Pawn(White), Tile("d3"): Empty(),
+        Tile("d4"): Empty(), Tile("d5"): Pawn(Black), Tile("d6"): Empty(),
+        Tile("d7"): Pawn(Black), Tile("d8"): Queen(Black), Tile("e1"): King(White),
+        Tile("e2"): Empty(), Tile("e3"): Empty(), Tile("e4"): Empty(),
+        Tile("e5"): Pawn(White), Tile("e6"): Empty(), Tile("e7"): Empty(),
+        Tile("e8"): King(Black), Tile("f1"): Bishop(White), Tile("f2"): Pawn(White),
+        Tile("f3"): Empty(), Tile("f4"): Empty(), Tile("f5"): Empty(),
+        Tile("f6"): Empty(), Tile("f7"): Pawn(Black), Tile("f8"): Bishop(Black),
+        Tile("g1"): Knight(White), Tile("g2"): Pawn(White), Tile("g3"): Empty(),
+        Tile("g4"): Empty(), Tile("g5"): Empty(), Tile("g6"): Empty(),
+        Tile("g7"): Pawn(Black), Tile("g8"): Empty(), Tile("h1"): Rook(White),
+        Tile("h2"): Pawn(White), Tile("h3"): Empty(), Tile("h4"): Empty(),
+        Tile("h5"): Empty(), Tile("h6"): Empty(), Tile("h7"): Pawn(Black),
+        Tile("h8"): Rook(Black),
+    }
+
+test_castling_board = {
+        Tile("a1"): Rook(White), Tile("a2"): Pawn(White), Tile("a3"): Empty(),
+        Tile("a4"): Empty(), Tile("a5"): Empty(), Tile("a6"): Empty(),
+        Tile("a7"): Pawn(Black), Tile("a8"): Rook(Black), Tile("b1"): Knight(White),
+        Tile("b2"): Pawn(White), Tile("b3"): Empty(), Tile("b4"): Empty(),
+        Tile("b5"): Empty(), Tile("b6"): Empty(), Tile("b7"): Pawn(Black),
+        Tile("b8"): Empty(), Tile("c1"): Bishop(White), Tile("c2"): Pawn(White),
+        Tile("c3"): Empty(), Tile("c4"): Empty(), Tile("c5"): Empty(),
+        Tile("c6"): Empty(), Tile("c7"): Pawn(Black), Tile("c8"): Empty(),
+        Tile("d1"): Empty(), Tile("d2"): Pawn(White), Tile("d3"): Empty(),
+        Tile("d4"): Empty(), Tile("d5"): Empty(), Tile("d6"): Empty(),
+        Tile("d7"): Pawn(Black), Tile("d8"): Empty(), Tile("e1"): King(White),
+        Tile("e2"): Pawn(White), Tile("e3"): Empty(), Tile("e4"): Empty(),
+        Tile("e5"): Empty(), Tile("e6"): Empty(), Tile("e7"): Pawn(White),
+        Tile("e8"): King(Black), Tile("f1"): Empty(), Tile("f2"): Pawn(White),
+        Tile("f3"): Empty(), Tile("f4"): Empty(), Tile("f5"): Empty(),
+        Tile("f6"): Empty(), Tile("f7"): Pawn(Black), Tile("f8"): Bishop(Black),
+        Tile("g1"): Empty(), Tile("g2"): Pawn(White), Tile("g3"): Empty(),
+        Tile("g4"): Empty(), Tile("g5"): Empty(), Tile("g6"): Empty(),
+        Tile("g7"): Pawn(Black), Tile("g8"): Empty(), Tile("h1"): Rook(White),
+        Tile("h2"): Pawn(White), Tile("h3"): Empty(), Tile("h4"): Empty(),
+        Tile("h5"): Empty(), Tile("h6"): Empty(), Tile("h7"): Pawn(Black),
+        Tile("h8"): Rook(Black),
+    }
+proper_testing_board = {
+        Tile("a1"): Rook(White), Tile("a2"): Pawn(White), Tile("a3"): Empty(),
+        Tile("a4"): Empty(), Tile("a5"): Rook(Black), Tile("a6"): Empty(),
+        Tile("a7"): Pawn(Black), Tile("a8"): Rook(Black), Tile("b1"): Knight(White),
+        Tile("b2"): Pawn(White), Tile("b3"): Empty(), Tile("b4"): Empty(),
+        Tile("b5"): Pawn(White), Tile("b6"): Empty(), Tile("b7"): Empty(),
+        Tile("b8"): Knight(Black), Tile("c1"): Bishop(White), Tile("c2"): Pawn(White),
+        Tile("c3"): Empty(), Tile("c4"): Empty(), Tile("c5"): Empty(),
+        Tile("c6"): Empty(), Tile("c7"): Pawn(Black), Tile("c8"): Bishop(Black),
+        Tile("d1"): Empty(), Tile("d2"): Pawn(White), Tile("d3"): Empty(),
+        Tile("d4"): Empty(), Tile("d5"): Empty(), Tile("d6"): Empty(),
+        Tile("d7"): Bishop(Black), Tile("d8"): Queen(Black), Tile("e1"): King(White),
+        Tile("e2"): Empty(), Tile("e3"): Empty(), Tile("e4"): Pawn(White),
+        Tile("e5"): Empty(), Tile("e6"): Empty(), Tile("e7"): Pawn(White),
+        Tile("e8"): King(Black), Tile("f1"): Bishop(White), Tile("f2"): Pawn(White),
+        Tile("f3"): Empty(), Tile("f4"): Empty(), Tile("f5"): Empty(),
+        Tile("f6"): Empty(), Tile("f7"): Pawn(Black), Tile("f8"): Bishop(Black),
+        Tile("g1"): Knight(White), Tile("g2"): Pawn(White), Tile("g3"): Empty(),
+        Tile("g4"): Empty(), Tile("g5"): Rook(Black), Tile("g6"): Empty(),
+        Tile("g7"): Pawn(Black), Tile("g8"): Queen(White), Tile("h1"): Rook(White),
+        Tile("h2"): Pawn(White), Tile("h3"): Empty(), Tile("h4"): Empty(),
+        Tile("h5"): Queen(White), Tile("h6"): Empty(), Tile("h7"): Empty(),
+        Tile("h8"): Rook(Black),
+        }
