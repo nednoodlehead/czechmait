1e3d87a6cf81851f6c741d5479c82b792a8b95a2
Neddy <nedninja.spencer@gmail.com>
2023-11-04 15:47:18 -0700

    official interface for the app lol

---
+++ main.py
@@ -0,0 +1,8 @@
+from board.color import Black, White
+from tests.test_analysis import test_notation_translation # call tests
+from board.chessboard import ChessBoard
+def main():
+    test_notation_translation()
+main() 
+
+
