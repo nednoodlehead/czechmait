3993da59536a0e0462ec66acde5f3cb00ed50216
NedNoodleHead <spencerbraak@gmail.com>
2023-12-06 16:05:59 -0800

    changed return type to print, it was sort of annoying having to print the function out all the time

--- tests/test_undo.py
+++ tests/test_undo.py
@@ -21,4 +21,4 @@ def test_three_move_undo():
     chs.undo_move()
     assert isinstance(x := chs.last_move[-1], LastMove) and x.undone_tile == Tile("d5") and isinstance(x.undone_occupant, Pawn) and x.original_tile == Tile("d7") and isinstance(x.original_occupant, Empty) and x.color == Black
 
-    return "Undo tests passed"
+    print("Undo tests passed")
