0720cf224e99269fe6483abb5bc061a994152ff9
NedNoodleHead <spencerbraak@gmail.com>
2023-12-08 10:20:55 -0800

    changed some code to get rid of the circular import :(

--- board/data_structure.py
+++ board/data_structure.py
@@ -3,7 +3,6 @@
 # as of pre november 2023 commits.
 from board.tile import Tile
 from board.color import Black, White
-from board.pieces import Piece, EnpassantRemnant
 
 # used to formalize showing additional information that comes with a move
 # so it can be either castling, enpassant or a two-sqaure pawn move (creates an enpassant remnant)
@@ -11,7 +10,7 @@ class Extra:
     pass
 
 class LastMove:
-    def __init__(self, undone_tile: Tile, undone_occupant: Piece, original_tile: Tile, original_occupant: Piece, color: White | Black, extra: Extra | None):
+    def __init__(self, undone_tile: Tile, undone_occupant, original_tile: Tile, original_occupant, color: White | Black, extra: Extra | None):
         """
         example: 1. d4
         param: undone_tile: Tile. d4
@@ -38,7 +37,6 @@ class Enpassant(Extra):
     def __init__(self, death_tile: Tile, color: White | Black):
         """
         Arguments:
-        starting_tile: Tile. Tile that the pawn begins on, so either 2nd or 7th rank
         death_tile: Tile. Tile that the pawn dies on, so either 4th or 5th rank
         color: Color. Black or White.
         """
@@ -63,21 +61,21 @@ class DoublePawnMove(Extra):
     # the pawn's nomal move (ex. d2 -> d4) is held outside of the extra type
     # this just contains the extra instruction for the move interperter to create an enpassant remnant
     # on the tile between the two (d3, in this case)
-    def __init__(self, tile: Tile, color: White | Black):
+    def __init__(self, tile: Tile, color: White | Black, type):
         """
         Arguments:
         tile: Tile. tile that the enpassant remnant is on. (Must be on 3rd or 6th rank)
         color: Color. White or Black
+        type: instance of the enpassant remnant
 
         Info:
         self.type: EnpassantRemnant. Instance of enpassant remnant to represent the tile
         """
         self.tile = tile
         self.color = color
-        self.type = EnpassantRemnant(self.color)
-
+       
 class Move:
-    def __init__(self, old_tile: Tile, new_tile: Tile, piece: Piece, extra: Extra | None):
+    def __init__(self, old_tile: Tile, new_tile: Tile, piece, extra: Extra | None):
         """
         Arguments:
         old_tile: Tile. Tile that the piece was on before
@@ -91,3 +89,5 @@ class Move:
         self.extra = extra  
     def __str__(self):
         return f"{self.piece} moves from {self.old_tile} to {self.new_tile}"
+    def __repr__(self):
+        return f"{self.piece} moves from {self.old_tile} to {self.new_tile}\n"
