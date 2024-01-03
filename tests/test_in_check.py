e934faf29272d0616b6c74c081ed1ebe362b0877
NedNoodleHead <spencer.braak@gmail.com>
2024-01-03 14:08:43 -0800

    updated tests with new logic

--- tests/test_in_check.py
+++ tests/test_in_check.py
@@ -3,6 +3,7 @@ from board.pieces import Rook, is_in_check, King, Bishop, Queen, Pawn
 import pytest
 from board.color import Black, White
 from board.empty import Empty
+from board.tile import Tile
 
 
 def test_check():
@@ -11,6 +12,7 @@ def test_check():
      chs.move_from_notation("d5", Black)
      chs.board["e1"] = Empty()
      chs.board["c4"] = King(White)
+     chs.white_king = Tile("c4")  # needed to specifically locate the king. done automatically in games :P
      chs.board["h4"] = Rook(Black)
      assert(is_in_check(chs, White) == True)
      chs.move_from_notation("Kb4", White)
